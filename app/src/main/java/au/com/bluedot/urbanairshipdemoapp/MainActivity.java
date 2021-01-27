caa62177800bde8364466e9eaf17605a0fd2781c
nehabluedot <neha@bluedot.io>
2021-01-28 01:01:20 +1100

    sendAuth changed to Initialize and startGeoTrigger API's

--- app/src/main/java/au/com/bluedot/urbanairshipdemoapp/MainActivity.java
+++ app/src/main/java/au/com/bluedot/urbanairshipdemoapp/MainActivity.java
@@ -1,14 +1,43 @@
 package au.com.bluedot.urbanairshipdemoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import au.com.bluedot.point.net.engine.ServiceManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+
+        Button init = findViewById(R.id.bInit);
+        init.setEnabled(
+                !ServiceManager.getInstance(getApplicationContext()).isBluedotServiceInitialized());
+    }
+
+    @Override
+    public void onClick(View v) {
+        int ID = v.getId();
+        MainApplication mainApplication = (MainApplication) getApplicationContext();
+        switch (ID) {
+            case R.id.bInit:
+                mainApplication.initPointSDK();
+                break;
+
+            case R.id.bReset:
+                mainApplication.reset();
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
--- app/src/main/java/au/com/bluedot/urbanairshipdemoapp/MainApplication.java
+++ app/src/main/java/au/com/bluedot/urbanairshipdemoapp/MainApplication.java
@@ -10,39 +10,24 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
-
+import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-
-import com.urbanairship.UAirship;
-import com.urbanairship.analytics.CustomEvent;
-
-import java.util.List;
-import java.util.Map;
-
-import au.com.bluedot.application.model.Proximity;
-import au.com.bluedot.point.ApplicationNotificationListener;
-import au.com.bluedot.point.ServiceStatusListener;
-import au.com.bluedot.point.net.engine.BDError;
-import au.com.bluedot.point.net.engine.BeaconInfo;
-import au.com.bluedot.point.net.engine.FenceInfo;
-import au.com.bluedot.point.net.engine.LocationInfo;
+import au.com.bluedot.point.net.engine.GeoTriggeringService;
+import au.com.bluedot.point.net.engine.InitializationResultListener;
 import au.com.bluedot.point.net.engine.ServiceManager;
-import au.com.bluedot.point.net.engine.ZoneInfo;
+import com.urbanairship.UAirship;
 
 import static android.app.Notification.PRIORITY_MAX;
 
 /**
  * Created by Adil Bhatti on 17/05/16.
  */
-public class MainApplication extends Application implements UAirship.OnReadyCallback, ServiceStatusListener, ApplicationNotificationListener {
+public class MainApplication extends Application implements UAirship.OnReadyCallback{
 
     private ServiceManager serviceManager;
-    private final String API_KEY = ""; //API key for the Point Demo App 
-    private final boolean RESTART_MODE = false;
+    private final String PROJECT_ID = "<PROJECT-ID>"; //ProjectID for the App  from Canvas
     private NotificationChannel notificationChannel = null;
-    private final String EVENT_PLACE_ENTERED = "bluedot_place_entered";
-    private final String EVENT_PLACE_EXITED = "bluedot_place_exited";
 
 
     @Override
@@ -59,19 +44,23 @@ public class MainApplication extends Application implements UAirship.OnReadyCall
     public void initPointSDK() {
         boolean locationPermissionGranted =
                 ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-        boolean backgroundPermissionGranted = (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-                || ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED;
 
-        if (locationPermissionGranted && backgroundPermissionGranted) {
+        if (locationPermissionGranted) {
             serviceManager = ServiceManager.getInstance(this);
 
-            if (!serviceManager.isBlueDotPointServiceRunning()) {
-                // Setting Notification for foreground service, required for Android Oreo and above.
-                // Setting targetAllAPIs to TRUE will display foreground notification for Android versions lower than Oreo
-                serviceManager.setForegroundServiceNotification(createNotification(), false);
-                serviceManager.sendAuthenticationRequest(API_KEY, this, RESTART_MODE);
+            if (!serviceManager.isBluedotServiceInitialized()) {
 
-
+                InitializationResultListener resultListener = bdError -> {
+                    String text = "Initialization Result ";
+                    if(bdError != null)
+                        text = text + bdError.getReason();
+                    else {
+                        text = text + "Success ";
+                        startGeoTrigger();
+                    }
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                };
+                serviceManager.initialize(PROJECT_ID, resultListener);
             }
         } else {
             requestPermissions();
@@ -96,86 +85,36 @@ public class MainApplication extends Application implements UAirship.OnReadyCall
                         .setNotificationChannel(notificationChannel.getName().toString());
             }
         }
-
     }
 
-
-    @Override
-    public void onCheckIntoFence(FenceInfo fenceInfo, ZoneInfo zoneInfo, LocationInfo locationInfo, Map<String, String> customDataMap, boolean b) {
-        // Toast.makeText(getApplicationContext(),"CheckIn "+zoneInfo.getZoneName(),Toast.LENGTH_SHORT).show();
-        sendCustomEvent(EVENT_PLACE_ENTERED, zoneInfo, customDataMap);
-
-
-    }
-
-    private void sendCustomEvent(String eventName, ZoneInfo zoneInfo, Map<String, String> customDataMap) {
-        sendCustomEvent(eventName, zoneInfo, -1, customDataMap);
-    }
-
-
-    private void sendCustomEvent(String eventName, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-//        name: "bluedot_place_exited"
-//        interaction_type: "location"
-//        interaction_id: zone_id
-//        properties: {
-//            bluedot_zone_name: <zone_name>
-//                    dwell_time: <dwell_time>
-//  <all custom data>
-//        }
-        CustomEvent.Builder builder = new CustomEvent.Builder(eventName);
-        builder.setInteraction("location", zoneInfo.getZoneId());
-        builder.addProperty("bluedot_zone_name", zoneInfo.getZoneName());
-        if (customDataMap != null && !customDataMap.isEmpty()) {
-            for (Map.Entry<String, String> data : customDataMap.entrySet()) {
-                builder.addProperty(data.getKey(), data.getValue());
+    void reset() {
+        serviceManager.reset(bdError -> {
+            String text = "Reset Finished ";
+            if(bdError != null)
+                text = text + bdError.getReason();
+            else {
+                text = text + "Success ";
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
-
-        }
-
-
-        if (dwellTime != -1) {
-            builder.addProperty("dwell_time", dwellTime);
-        }
-        CustomEvent event = builder.build();
-
-        System.out.println("-- event data : " + event.toJsonValue());
-        event.track();
+            Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+        });
     }
 
-    @Override
-    public void onCheckedOutFromFence(FenceInfo fenceInfo, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-        sendCustomEvent(EVENT_PLACE_EXITED, zoneInfo, dwellTime, customDataMap);
-    }
+    void startGeoTrigger() {
+        Notification notification = createNotification();
 
-    @Override
-    public void onCheckIntoBeacon(BeaconInfo beaconInfo, ZoneInfo zoneInfo, LocationInfo locationInfo, Proximity proximity, Map<String, String> customDataMap, boolean b) {
-        sendCustomEvent(EVENT_PLACE_ENTERED, zoneInfo, customDataMap);
-    }
+        GeoTriggeringService.builder()
+                .notification(notification)
+                .start(this, geoTriggerError -> {
+                    if (geoTriggerError != null) {
+                        Toast.makeText(getApplicationContext(),"Error in starting GeoTrigger"+geoTriggerError.getReason(),Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Toast.makeText(getApplicationContext(),"GeoTrigger started successfully",Toast.LENGTH_LONG).show();
 
-    @Override
-    public void onCheckedOutFromBeacon(BeaconInfo beaconInfo, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-        sendCustomEvent(EVENT_PLACE_EXITED, zoneInfo, dwellTime, customDataMap);
-    }
-
-    @Override
-    public void onBlueDotPointServiceStartedSuccess() {
-        System.out.println("-- onBlueDotPointServiceStartedSuccess");
-        serviceManager.subscribeForApplicationNotification(this);
-    }
-
-    @Override
-    public void onBlueDotPointServiceStop() {
-        serviceManager.unsubscribeForApplicationNotification(this);
-    }
-
-    @Override
-    public void onBlueDotPointServiceError(BDError bdError) {
-
-    }
-
-    @Override
-    public void onRuleUpdate(List<ZoneInfo> list) {
-
+                });
     }
 
     /**
